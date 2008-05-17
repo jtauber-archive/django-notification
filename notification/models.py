9baff31824c212f8aff90a286163930ccc7e5475
James Tauber <jtauber@jtauber.com>
2008-05-17 02:36:05 +0000

    initial models for django-notification
    git-svn-id: http://django-notification.googlecode.com/svn/trunk@3 590c3fc9-4838-0410-bb95-17a0c9b37ca9

--- notification/models.py
+++ notification/models.py
@@ -1,3 +1,40 @@
+import datetime
+
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+class NoticeType(models.Model):
+    
+    label = models.CharField(max_length=50)
+    
+    def __unicode__(self):
+        return self.label
+    
+    class Admin:
+        pass
+
+
+class Notice(models.Model):
+    
+    user = models.ForeignKey(User)
+    message = models.TextField()
+    notice_type = models.ForeignKey(NoticeType)
+    added = models.DateTimeField(default=datetime.datetime.now)
+    
+    class Admin:
+        pass
+
+
+def create_notice_type(label):
+    notice_type, created = NoticeType.objects.get_or_create(label=label)
+    if created:
+        print "Created %s NoticeType" % label
+
+
+def create(user, notice_type_label, message):
+    notice_type = NoticeType.objects.get(label=notice_type_label)
+    notice = Notice(user=user, message=message, notice_type=notice_type)
+    notice.save()
+    return notice
+    
