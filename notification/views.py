6ea1d7ae4b6cae62bd40398258b8757fa64bfdd6
James Tauber <jtauber@jtauber.com>
2008-05-22 07:41:55 +0000

    moved view from pinax to here
    git-svn-id: http://django-notification.googlecode.com/svn/trunk@7 590c3fc9-4838-0410-bb95-17a0c9b37ca9

--- notification/views.py
+++ notification/views.py
@@ -1,1 +1,12 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from notification.models import notices_for
+
+def notices(request):
+    if request.user.is_authenticated():
+        notices = notices_for(request.user)
+    else:
+        notices = None
+    return render_to_response("notification/notices.html", {
+        "notices": notices,
+    }, context_instance=RequestContext(request))
