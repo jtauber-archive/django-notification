1fe7932a418fba339f78a42ceac4212407c50c01
James Tauber <jtauber@jtauber.com>
2008-05-22 10:44:46 +0000

    notices view how includes notice types
    git-svn-id: https://django-notification.googlecode.com/svn/trunk@10 590c3fc9-4838-0410-bb95-17a0c9b37ca9

--- notification/views.py
+++ notification/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from notification.models import notices_for
+from notification.models import notices_for, NoticeType
 
 def notices(request):
+    notice_types = NoticeType.objects.all()
     if request.user.is_authenticated():
         notices = notices_for(request.user)
     else:
         notices = None
     return render_to_response("notification/notices.html", {
         "notices": notices,
+        "notice_types": notice_types,
     }, context_instance=RequestContext(request))
