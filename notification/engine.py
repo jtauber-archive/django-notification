7570991637071df21ea319b9658855a63a54f8bb
Brian Rosner <brosner@gmail.com>
2008-10-17 20:31:28 +0000

    Encode/decode pickle data before and coming out the database. pickle doesn't gurantee a bytestream when done serializing.
    git-svn-id: https://django-notification.googlecode.com/svn/trunk@107 590c3fc9-4838-0410-bb95-17a0c9b37ca9

--- notification/engine.py
+++ notification/engine.py
@@ -38,7 +38,7 @@ def send_all():
 
     try:
         for queued_batch in NoticeQueueBatch.objects.all():
-            notices = pickle.loads(str(queued_batch.pickled_data))
+            notices = pickle.loads(str(queued_batch.pickled_data).decode("base64"))
             for user, label, extra_context, on_site in notices:
                 user = User.objects.get(pk=user)
                 logging.info("emitting notice to %s" % user)
