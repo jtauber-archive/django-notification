b664582f911340cfdfd492c6fd3f509225b2c0c7
Brian Rosner <brosner@gmail.com>
2008-12-10 20:39:06 +0000

    Catch exceptions while processing the queue and handle them nicely and e-mail them to ADMINS.
    git-svn-id: http://django-notification.googlecode.com/svn/trunk@119 590c3fc9-4838-0410-bb95-17a0c9b37ca9

--- notification/engine.py
+++ notification/engine.py
@@ -1,6 +1,8 @@
 
+import sys
 import time
 import logging
+import traceback
 
 try:
     import cPickle as pickle
@@ -8,7 +10,9 @@ except ImportError:
     import pickle
 
 from django.conf import settings
+from django.core.mail import mail_admins
 from django.contrib.auth.models import User
+from django.contrib.sites.models import Site
 
 from lockfile import FileLock, AlreadyLocked, LockTimeout
 
@@ -48,6 +52,16 @@ def send_all():
                 sent += 1
             queued_batch.delete()
             batches += 1
+    except:
+        # get the exception
+        exc_class, e, t = sys.exc_info()
+        # email people
+        current_site = Site.objects.get_current()
+        subject = "[%s emit_notices] %r" % (current_site.name, e)
+        message = "%s" % ("\n".join(traceback.format_exception(*sys.exc_info())),)
+        mail_admins(subject, message, fail_silently=True)
+        # log it as critical
+        logging.critical("an exception occurred: %r" % e)
     finally:
         logging.debug("releasing lock...")
         lock.release()
