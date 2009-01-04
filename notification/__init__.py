8bc8ee350cbe08e7dc3a171d6533bab289b17080
Brian Rosner <brosner@gmail.com>
2009-01-04 23:32:11 +0000

    Modified the version generation to ignore the final designation.
    git-svn-id: https://django-notification.googlecode.com/svn/trunk@134 590c3fc9-4838-0410-bb95-17a0c9b37ca9

--- notification/__init__.py
+++ notification/__init__.py
@@ -1,6 +1,9 @@
 VERSION = (0, 1, 0, "pre")
 
 def get_version():
-    return "%s.%s.%s%s" % (VERSION[0], VERSION[1], VERSION[2], VERSION[3])
+    if VERSION[3] != "final":
+        return "%s.%s.%s%s" % (VERSION[0], VERSION[1], VERSION[2], VERSION[3])
+    else:
+        return "%s.%s.%s" % (VERSION[0], VERSION[1], VERSION[2])
 
 __version__ = get_version()
