297359692403a7ad13f1d40b52b30df5d33aded8
Brian Rosner <brosner@gmail.com>
2009-01-04 23:17:16 +0000

    Updated the version generating to be more specific and format nicely.
    git-svn-id: http://django-notification.googlecode.com/svn/trunk@131 590c3fc9-4838-0410-bb95-17a0c9b37ca9

--- notification/__init__.py
+++ notification/__init__.py
@@ -1,2 +1,6 @@
-VERSION = (0, 1, 'pre')
-__version__ = '.'.join(map(str, VERSION))
+VERSION = (0, 1, 0, "pre")
+
+def get_version():
+    return "%s.%s.%s%s" % (VERSION[0], VERSION[1], VERSION[2], VERSION[3])
+
+__version__ = get_version()
