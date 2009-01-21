f97c8470331886382aeb4cd94b9ba6e3c22187bf
James Tauber <jtauber@jtauber.com>
2009-01-21 11:51:17 +0000

    fixed missing colon
    git-svn-id: http://django-notification.googlecode.com/svn/trunk@151 590c3fc9-4838-0410-bb95-17a0c9b37ca9

--- notification/models.py
+++ notification/models.py
@@ -26,7 +26,7 @@ from django.utils.translation import ugettext, get_language, activate
 
 # favour django-mailer but fall back to django.core.mail
 if 'mailer' in settings.INSTALLED_APPS
-    from mailer import send_mail
+    from mailer import send_mail:
 else:
     from django.core.mail import send_mail
 
