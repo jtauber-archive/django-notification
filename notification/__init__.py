9bb89dd03f2c2d82320b020dbe64bc292e8a2b28
Brian Rosner <brosner@gmail.com>
2010-01-04 23:34:26 -0700

    0.2.0.dev2

--- notification/__init__.py
+++ notification/__init__.py
@@ -1,10 +1,10 @@
-VERSION = (0, 2, 0, "dev", 1)
+VERSION = (0, 2, 0, "dev", 2)
 
 def get_version():
     if VERSION[3] == "final":
         return "%s.%s.%s" % (VERSION[0], VERSION[1], VERSION[2])
     elif VERSION[3] == "dev":
-        return "%s.%s.%s%s%s" % (VERSION[0], VERSION[1], VERSION[2], VERSION[3], VERSION[4])
+        return "%s.%s.%s.%s%s" % (VERSION[0], VERSION[1], VERSION[2], VERSION[3], VERSION[4])
     else:
         return "%s.%s.%s%s" % (VERSION[0], VERSION[1], VERSION[2], VERSION[3])
 
