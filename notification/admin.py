f9a0851fdd545d2b53ad6a6d35cd48b4b91e791e
Mathieu Pillard <m.pillard@liberation.fr>
2010-03-31 14:58:27 +0200

    Display batches in the admin for debugging
    Signed-off-by: Brian Rosner <brosner@gmail.com>

--- notification/admin.py
+++ notification/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from notification.models import NoticeType, NoticeSetting, Notice, ObservedItem
+from notification.models import NoticeType, NoticeSetting, Notice, ObservedItem, NoticeQueueBatch
 
 class NoticeTypeAdmin(admin.ModelAdmin):
     list_display = ('label', 'display', 'description', 'default')
@@ -10,7 +10,7 @@ class NoticeSettingAdmin(admin.ModelAdmin):
 class NoticeAdmin(admin.ModelAdmin):
     list_display = ('message', 'recipient', 'sender', 'notice_type', 'added', 'unseen', 'archived')
 
-
+admin.site.register(NoticeQueueBatch)
 admin.site.register(NoticeType, NoticeTypeAdmin)
 admin.site.register(NoticeSetting, NoticeSettingAdmin)
 admin.site.register(Notice, NoticeAdmin)
